4b776247b4bd79a6b2bcd47a106bc2cb44a7f81c
Bambang Alibowo <Bambang Alibowo>
2016-07-01 17:41:21 +0700

    komit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,105 @@
+@extends('master.main')
+
+@section('page-content')
+<section id="atas">
+		<div id='header-wrapper'>
+		<div id="header" class="container">
+			
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="staff" class="container">
+
+			<div class="title">
+				<h2>Pilih genre ceritamu</h2>
+				<span>jalan ceritamu adalah bagian dari jalan cerita kita semua</span> </div>
+				<div class="row">
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="..." alt="...">
+				      <div class="caption">
+				        <h3>Thumbnail label</h3>
+				        <p>...</p>
+				        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				      </div>
+				    </div>
+				  </div>
+
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="..." alt="...">
+				      <div class="caption">
+				        <h3>Thumbnail label</h3>
+				        <p>...</p>
+				        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				      </div>
+				    </div>
+				  </div>
+
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="..." alt="...">
+				      <div class="caption">
+				        <h3>Thumbnail label</h3>
+				        <p>...</p>
+				        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+
+		</div>
+	</div>	
+
+</section>
+
+<section id="tengah">
+		<div id='header-wrapper'>
+		<div id="header" class="container">
+			
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="staff" class="container">
+
+			<div class="title">
+				<h2>Silahkan pilih genre ceritamu</h2>
+				<span>jalan ceritamu adalah bagian dari jalan cerita kita semua</span> </div>
+				<div class="boxA"><img src="images/pic01.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxB"><img src="images/pic02.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxC"><img src="images/pic03.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxA"><img src="images/pic01.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxB"><img src="images/pic02.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxC"><img src="images/pic03.jpg" width="300" height="450" alt="" /></div>
+			</div>
+
+		</div>
+	</div>	
+
+</section>
+
+<section id="bawah">
+	<div id="wrapper">
+		<div id="staff" class="container">
+
+			<div class="title">
+				<div class="boxA"><img src="images/pic01.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxB"><img src="images/pic02.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxC"><img src="images/pic03.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxA"><img src="images/pic01.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxB"><img src="images/pic02.jpg" width="300" height="450" alt="" /></div>
+				<div class="boxC"><img src="images/pic03.jpg" width="300" height="450" alt="" /></div>
+			</div>
+
+		</div>
+	</div>	
+
+</section>
+
+
+
+
+
+
+@endsection
---
+++ resources/views/master/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title></title>
+		
+		<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+		<link href="css/font.css" rel="stylesheet" type="text/css" media="all" />
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+		<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+	</head>
+	<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+	  <div class="container">
+	    ...
+	  </div>
+	</nav>
+
+	
+	@yield('page-content')
+		
+	
+		
+	
+	</body>
+
+	<footer>
+	
+	</footer>
+</html>
